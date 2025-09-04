69621bc3cdfea84ac75d56cfb15c113491c93223
Sohail Akbar <140832169+Sohail-Akbar@users.noreply.github.com>
2025-09-04 15:02:37 +0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'login_system');
+
+// Api Path
+define("API_PATH", "/projects/login-system/api");
+
+define("ENV", "local");
